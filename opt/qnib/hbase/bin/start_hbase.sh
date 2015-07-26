913110072544a4236bef7c3cbf6f8eacb221b0e9
Christian Kniep <christian@qnib.org>
2015-07-26 14:23:50 +0200

    argument order of hbase-start fixed

--- opt/qnib/hbase/bin/start_hbase.sh
+++ opt/qnib/hbase/bin/start_hbase.sh
@@ -3,7 +3,7 @@
 export JAVA_HOME=/usr/lib/jvm/jre-1.7.0-openjdk/
 trap "echo stopping hbase;/usr/lib/hbase/bin/hbase-daemon.sh master stop>>/var/log/hbase-stop.log 2>&1; exit" HUP INT TERM EXIT
 echo "starting hbase"
-/usr/lib/hbase/bin/hbase-daemon.sh master start &
+/usr/lib/hbase/bin/hbase-daemon.sh start master &
 while true
 do
   sleep 1
