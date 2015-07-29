a9ea85f7cbc9388e3c1be87a7fd07cc8c8051fea
Christian Kniep <ckniep@gaikai.com>
2015-07-29 14:53:14 +0200

    fix which does not start

--- opt/qnib/hbase/bin/start_hbase.sh
+++ opt/qnib/hbase/bin/start_hbase.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ## stolen from: https://github.com/PeterGrace/opentsdb-docker/blob/master/start_hbase.sh
 export JAVA_HOME=/usr/lib/jvm/jre-1.7.0-openjdk/
-trap "echo stopping hbase;/usr/lib/hbase/bin/hbase-daemon.sh master stop>>/var/log/hbase-stop.log 2>&1; exit" HUP INT TERM EXIT
+trap "echo stopping hbase;/usr/lib/hbase/bin/hbase-daemon.sh stop master>>/var/log/hbase-stop.log 2>&1; exit" HUP INT TERM EXIT
 echo "starting hbase"
-/usr/lib/hbase/bin/hbase-daemon.sh master start &
+/usr/lib/hbase/bin/hbase-daemon.sh start master &
 while true
 do
   sleep 1
